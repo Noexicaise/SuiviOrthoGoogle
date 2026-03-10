183abf93c9053c067a33de5fa94e5a8b11721e7f
Noexicaise <julien.nicaise@gmail.com>
2026-03-10 11:28:27 +0100

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,20 @@
+const CACHE_NAME = 'maya-v1';
+const ASSETS = [
+  'ortho.html',
+  'manifest.json',
+  'https://cdn.tailwindcss.com'
+];
+
+// Installation : Mise en cache des fichiers
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+// Stratégie : Réseau d'abord, sinon Cache
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    fetch(e.request).catch(() => caches.match(e.request))
+  );
+});
